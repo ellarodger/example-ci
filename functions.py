2b5d4b71bb6f9f807a17de3b8a4e41dc723da2ed
Ella Rodger <ellarodger@outlook.com.au>
2024-03-20 16:22:54 +0800

    Introduced Errors

--- functions.py
+++ functions.py
@@ -3,14 +3,12 @@ def add(a, b):
 
 
 def subtract(a, b):
-    return a - b  # <--- fix this in step 7
+    return a + b  # <--- fix this in step 7
 
 
 def multiply(a, b):
-    return a * b
+    return a / b
 
 
 def convert_fahrenheit_to_celsius(fahrenheit):
-    if fahrenheit <= -600:
-        raise AssertionError
-    return multiply(subtract(fahrenheit, 32), 5 / 9)  # <-- Fix this in step 7
+    return multiply(subtract(fahrenheit, 32), 5 * 9)  # <-- Fix this in step 7
